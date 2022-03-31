2ee47e7820d184d5f015d881be0adf3af0038f3f
ahmaadzaid7@gmail.com <ahmaadzaid7@gmail.com>
2022-03-31 08:55:33 +0300

    add login page design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,28 +18,49 @@
                             <div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img
                                         src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40"
                                         alt="logo"></a>
-                                <h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">إدارة صيانة محطات التحويل الرئيسية</h1>
+                                        <h3 class="m-2">إدارة صيانة محطات التحويل
+                                        الرئيسية</h5>
                             </div>
                             <div class="card-sigin">
                                 <div class="main-signup-header">
                                     <h2 class="text-dark">مرحبا بك</h2>
                                     <h5 class="font-weight-semibold mb-4">تسجيل الدخول</h5>
-                                    <form action="{{ route('login') }}">
+                                       <!-- Session Status -->
+                             <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                            <!-- Validation Errors -->
+                        <x-auth-validation-errors class="mb-4 text-danger" :errors="$errors" />
+
+                                    <form method="post" action="{{ route('login') }}">
+                                        @csrf
                                         <div class="form-group">
-                                            <label>البريد الالكتروني</label> <input class="form-control"
-                                                placeholder="Enter your email" type="text">
+                                            <label>البريد الالكتروني</label> 
+                                                <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required
+                                            autofocus />
                                         </div>
                                         <div class="form-group">
-                                            <label>كلمة المرور</label> <input class="form-control"
-                                                placeholder="Enter your password" type="password">
-                                        </div><button class="btn btn-dark btn-block">Sign In</button>
-                                 
-                                    </form>
-                                    <div class="main-signin-footer mt-5">
-                                        <p><a href="">نسيت كلمة المرور؟</a></p>
-                                        <p>Don't have an account? <a href="{{ url('/' . $page='signup') }}">Create
-                                                an Account</a></p>
+                                            <label>كلمة المرور</label> 
+                                            <x-input id="password" class="form-control" type="password" name="password" required
+                                            autocomplete="current-password" />
+                                        </div><button class="btn btn-dark btn-block">تسجيل الدخول</button>
+                                      <!-- Remember Me -->
+                                    <div class="block mt-4">
+                                        <label for="remember_me" class="inline-flex items-center">
+                                            <input id="remember_me" type="checkbox"
+                                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                name="remember">
+                                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                        </label>
                                     </div>
+                                    </form>
+                                    <div class="flex items-center justify-end mt-4">
+                                 @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                {{ __('هل نسيت كلمة المرور؟') }}
+                                </a>
+                                @endif
+
+                            </div>
                                 </div>
                             </div>
                         </div>
@@ -59,6 +80,7 @@
     </div><!-- End -->
 </div>
 </div>
+
 @endsection
 @section('js')
 @endsection
