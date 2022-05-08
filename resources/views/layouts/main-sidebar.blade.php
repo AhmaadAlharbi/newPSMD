c7c3fdebae164dbe0f976af247e455a8306e19a1
ahmaadzaid7@gmail.com <ahmaadzaid7@gmail.com>
2022-05-08 09:04:41 +0300

    add MEW logo

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -1,15 +1,9 @@
 <!-- main-sidebar -->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar sidebar-scroll">
-    <div class="main-sidebar-header active">
-        <a class="desktop-logo logo-light active" href="{{ url('/' . $page='index') }}"><img
-                src="{{URL::asset('assets/img/brand/logo.png')}}" class="main-logo" alt="logo"></a>
-        <a class="desktop-logo logo-dark active" href="{{ url('/' . $page='index') }}"><img
-                src="{{URL::asset('assets/img/brand/logo-white.png')}}" class="main-logo dark-theme" alt="logo"></a>
-        <a class="logo-icon mobile-logo icon-light active" href="{{ url('/' . $page='index') }}"><img
-                src="{{URL::asset('assets/img/brand/favicon.png')}}" class="logo-icon" alt="logo"></a>
-        <a class="logo-icon mobile-logo icon-dark active" href="{{ url('/' . $page='index') }}"><img
-                src="{{URL::asset('assets/img/brand/favicon-white.png')}}" class="logo-icon dark-theme" alt="logo"></a>
+<div class="main-sidebar-header active">
+        <img class="m-2 p-2" src="{{URL::asset('image/logo.png')}}" alt="logo">
+    </div>
     </div>
     <div class="main-sidemenu">
         <div class="app-sidebar__user clearfix">
@@ -19,7 +13,7 @@
                 <div class="user-info">
                     <h4 class="font-weight-semibold mt-3 mb-0">{{Auth::user()->name}}</h4>
                     <span class="mb-0 text-muted">{{Auth::user()->email}}</span>
-                    <a class="btn btn-outline-success p-3 d-block" href="">{{Auth::user()->name}}
+                    <a class="btn btn-outline-success p-3 d-block" href="/dashboard/user/query_section_id=Auth::user()->section_id}}">{{Auth::user()->name}}
                         page</a>
 
                 </div>
