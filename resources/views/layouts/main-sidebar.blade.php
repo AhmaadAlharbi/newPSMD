7d0417f642701723d26ade0fad1277c7b554cfc6
AhmadAlharbi <ahmaadzaid7@gmail.com>
2022-11-17 07:33:43 +0300

    update sidebar

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -39,6 +39,7 @@
                     </svg><span class="side-menu__label">الرئيسية</span>
                 </a>
                 @if(Auth::user()->role === 'admin')
+            <li class="slide">
                 <a class="side-menu__item"
                     href="{{ '/dashboard/' . $is_admin . '/query_section_id=' . Auth::user()->section_id .'/general-check' }}"><svg
                         xmlns=" http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
@@ -48,7 +49,8 @@
                             d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
                     </svg><span class="side-menu__label">صفحة استلام المحطات</span>
                 </a>
-                @endif
+            </li>
+            @endif
             </li>
             @auth
             {{--TS Role--}}
